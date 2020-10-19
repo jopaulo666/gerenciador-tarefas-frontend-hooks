df3218d275d77ff0ad3ee40f5b363d0d589aae6a
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-10-19 12:25:49 -0300

    Formulário de login usando hooks

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,85 +1,57 @@
-import React, { Component } from 'react';
-import AuthService from '../api/AuthService';
+import React, { useContext, useState } from 'react';
 import Alert from './Alert';
 import { Redirect } from 'react-router-dom';
+import { AuthContext } from '../hooks/useAuth';
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
+const Login =  () => {
+    const auth = useContext(AuthContext);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-        this.state = {
-            username: "",
-            password: "",
-            alert: null,
-            processing: false,
-            loggedIn: false
-        }
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleInputChanged = this.handleInputChanged.bind(this);     
-    }
-
-    handleSubmit(event) {
+    const  handleSubmit = (event) => {
         event.preventDefault();
-        AuthService.login(this.state.username, this.state.password,
-            success => {
-                if (success) {
-                    this.setState({ loggedIn: true, processing: false});
-                    this.props.onLoginSuccess();
-                } else {
-                    this.setState({ alert: "Login inválido", processing: false });
-                }
-        });
+        auth.login(username, password);
+    }
+    
+    if (auth.isAuthenticated()) {
+        return <Redirect to="/" />
     }
 
-    handleInputChanged(event) {
-        const field = event.target.name;
-        const value = event.target.value;
-        this.setState({[field]: value});
-    }
+    return (
+        <div className="text-center">                
+            <form className="form-signin" onSubmit={handleSubmit}>
+                <img className="mb-4" src="/images/pngwing.com.png" alt="" width="72" height="72"></img>
+                <h1 className="h3 mb-3 font-weight-normal">Faça login</h1>
+                {auth.error && <Alert message={auth.error} />}
+                <div className="form-group">
+                    <label className="sr-only" htmlFor="username">Usuário: </label>
+                    <input 
+                        type="text"
+                        className="form-control"
+                        onChange={(event) => setUsername(event.target.value)} 
+                        value={username}
+                        placeholder="Nome de usuário"/>
+                </div>
 
-    render() {
-        if (AuthService.isAuthenticated() || this.state.loggedIn) {
-            return <Redirect to="/" />
-        }
-
-        return (
-            <div className="text-center">                
-                <form className="form-signin" onSubmit={this.handleSubmit}>
-                    <img className="mb-4" src="/images/pngwing.com.png" alt="" width="72" height="72"></img>
-                    <h1 className="h3 mb-3 font-weight-normal">Faça login</h1>
-                    {this.state.alert !== null ? <Alert message={this.state.alert} /> : "" }
-                    <div className="form-group">
-                        <label className="sr-only" htmlFor="username">Usuário: </label>
-                        <input 
-                            type="text"
-                            className="form-control"
-                            onChange={this.handleInputChanged} 
-                            value={this.state.username}
-                            name="username"
-                            placeholder="Nome de usuário"/>
-                    </div>
-
-                    <div className="form-group">
-                        <label className="sr-only" htmlFor="password">Senha: </label>
-                        <input 
-                            type="password" 
-                            className="form-control"
-                            onChange={this.handleInputChanged} 
-                            value={this.state.password}
-                            name="password"
-                            placeholder="Senha de usuário"/>
-                    </div>
-                    <button 
-                        type="submit"
-                        className="btn btn-lg btn-primary btn-block"
-                        disabled={this.state.processing}>Login</button>
-                    <p className="mt-5 mb-3 text-muted">&copy; 2020 - João Paulo da Mata Mendes</p>
-                </form>
-                
-            </div>
-        );
-    }
+                <div className="form-group">
+                    <label className="sr-only" htmlFor="password">Senha: </label>
+                    <input 
+                        type="password" 
+                        className="form-control"
+                        onChange={(event) => setPassword(event.target.value)} 
+                        value={password}
+                        placeholder="Senha de usuário"/>
+                </div>
+                <button 
+                    type="submit"
+                    className="btn btn-lg btn-primary btn-block"
+                    disabled={auth.processing}>Login</button>
+                <p className="mt-5 mb-3 text-muted">&copy; 2020 - João Paulo da Mata Mendes</p>
+            </form>
+            
+        </div>
+    );
+    
 }
 
 export default Login;
