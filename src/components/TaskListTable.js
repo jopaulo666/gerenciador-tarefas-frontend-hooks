2681fa348f0a6a12a77782becebb5baddd7042fc
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-10-23 09:13:15 -0300

    Alteração de status de uma tarefa usando hooks

--- src/components/TaskListTable.js
+++ src/components/TaskListTable.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { Redirect } from 'react-router-dom';
@@ -11,38 +11,58 @@ import { AuthContext } from '../hooks/useAuth';
 const TaskListTable = () => {
     const auth = useContext(AuthContext);
     const tasks = useTasks();
+    const [ editId, setEditId ] = useState(0);
 
     useEffect(() => {
         if (auth.credentials.username !== null) {
-            tasks.list();            
+            tasks.list();
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [auth.credentials]);
 
     const onDeleteHandler = (taskToDelete) => {
-        if (window.confirm("Deseja realmente excluir essa tarefa?")) {
-            tasks.remove(taskToDelete);           
+        if (window.confirm("Deseja mesmo excluir esta tarefa?")) {
+            tasks.remove(taskToDelete);
         }
+    }
+
+    const onEditHandler = (taskToEdit) => {
+        setEditId(taskToEdit.id);
+    }
+
+    const onStatusChangeHandler = (taskToUpdate) => {
+        taskToUpdate.done = !taskToUpdate.done;
+        tasks.save(taskToUpdate, true);
     }
 
     useEffect(() => {
         if (tasks.taskRemoved !== null) {
-            toast.success(`Tarefa ${tasks.taskUpdated.id} foi marcada como ${!tasks.taskUpdated.done ? "não" : ""} concluída!`, 
-                { position: toast.POSITION.BOTTOM_LEFT});
-            tasks.clearTaskUpdated();          
+            toast.success(`Tarefa ${tasks.taskRemoved.description} excluída com sucesso!`, 
+                { position: toast.POSITION.TOP_CENTER});
+            tasks.clearTaskRemoved();
+        }
+
+        if (tasks.taskUpdated !== null) {
+            toast.success(`Tarefa ${tasks.taskUpdated.description} foi marcada como ${!tasks.taskUpdated.done ? "não" : ""} concluída!`, 
+                { position: toast.POSITION.TOP_CENTER});
+            tasks.clearTaskUpdated();
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [tasks.taskRemoved]);
-    
+    }, [ tasks.taskRemoved, tasks.taskUpdated ]);
+
     if (!auth.isAuthenticated()) {
         return <Redirect to="/login" />
+    }
+
+    if (editId > 0) {
+        return <Redirect to={`/form/${editId}`} />
     }
 
     return (
         <>
             <h1>Lista de Tarefas</h1>
-            { tasks.error && <Alert message={tasks.error} /> }
-            { tasks.processing ? <Spinner /> :
+            {tasks.error && <Alert message={tasks.error} />}
+            {tasks.processing ? <Spinner /> :
                 <table className="table table-striped">
                     <thead className="thead-dark">
                         <tr>
@@ -51,50 +71,46 @@ const TaskListTable = () => {
                             <th scope="col">Data</th>
                             <th scope="col">Ações</th>
                         </tr>
-                    </thead>     
-                   
-                    <tbody>               
-                        {tasks.taskList.lenght === 0 ? <tr><td colSpan="4">Não existe tarefas cadastradas</td></tr> :                 
+                    </thead>
+
+                    <tbody>
+                        {tasks.taskList.length === 0 ? <tr><td colSpan="4">Sem tarefas cadastradas no momento!</td></tr> :
                             (
                                 tasks.taskList.map(task =>
                                     <tr key={task.id}>
                                         <td>
-                                            <input 
-                                                type="checkbox" 
+                                            <input type="checkbox"
                                                 checked={task.done}
-                                                onChange={() => false}/>
+                                                onChange={() => onStatusChangeHandler(task)} />
                                         </td>
-                                        <td>{ task.done ? <s>{task.description}</s> : task.description}</td>
-                                        <td>{ task.done ? 
-                                            <s><Moment format="DD/MM/YYYY">{task.whenToDo}</Moment> </s> 
-                                            : <Moment format="DD/MM/YYYY">{task.whenToDo}</Moment>}
+                                        <td>{task.done ? <s>{task.description}</s> : task.description}</td>
+                                        <td>{task.done ?
+                                            <s><Moment format="DD/MM/YYYY">{task.whenToDo}</Moment></s>
+                                            : <Moment format="DD/MM/YYYY">{task.whenToDo}</Moment>
+                                        }
                                         </td>
                                         <td>
-                                            <input 
-                                                type="button" 
-                                                className="btn btn-primary" 
+                                            <input
+                                                type="button"
+                                                className="btn btn-primary"
                                                 value="Editar"
-                                                title="Editar"
-                                                onClick={() => false}/>
-                                            &nbsp;
-                                            <input 
-                                                type="button" 
-                                                className="btn btn-danger" 
+                                                onClick={() => onEditHandler(task)} />
+                                    &nbsp;
+                                    <input
+                                                type="button"
+                                                className="btn btn-danger"
                                                 value="Excluir"
-                                                title="Excluir"
-                                                onClick={() => onDeleteHandler(task)}/>    
+                                                onClick={() => onDeleteHandler(task)} />
                                         </td>
                                     </tr>
-                                )                                   
-                            )
-                        }
+                                )
+                            )}
                     </tbody>
                 </table>
             }
-            <ToastContainer autoClose={2000} />
+            <ToastContainer autoClose={2500} />
         </>
     );
-    
 }
 
 export default TaskListTable;
